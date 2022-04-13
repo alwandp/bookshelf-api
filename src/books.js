efc046c49aab9533657fa6d7ac53d6a56d345956
Alwan Dwi Putra <alwandp34@gmail.com>
2022-04-13 13:30:38 +0700

    first commit

---
+++ src/books.js
@@ -0,0 +1,15 @@
+/*
+  {
+    "name": string,
+    "year": number,
+    "author": string,
+    "summary": string,
+    "publisher": string,
+    "pageCount": number,
+    "readPage": number,
+    "reading": boolean
+  }
+*/
+const books = [];
+
+module.exports = books;
